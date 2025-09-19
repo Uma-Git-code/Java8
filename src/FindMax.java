d886fbffbcaea100c4b048b1ce3006108b1d4159
abc <abc@example.com>
2025-09-19 18:20:39 -0500

    Starting Lambda

--- src/FindMax.java
+++ src/FindMax.java
@@ -2,5 +2,9 @@ public class FindMax {
     public static void main(String aa[]){
         System.out.println("Hello Lambda1");
         System.out.println("Hello Lambda2");
+        System.out.println("Hello Lambda3");
+    }
+    public void show(){
+        System.out.println("I am show 1 ");
     }
 }
